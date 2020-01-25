4695000fde4cf9a182db294f9ec708f5e3fe6910
swhite-zhang <351168391@qq.com>
2020-01-25 16:29:16 +0800

    add database

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -1,5 +1,5 @@
 //index.js
-const app = getApp()
+const app = getApp();
 
 Page({
   data: {
@@ -8,89 +8,140 @@ Page({
     logged: false,
     takeSession: false,
     requestResult: "",
-    hasUserInfo: false,
     canIUse: wx.canIUse("button.open-type.getUserInfo"),
     getNum: false,
     getName: false,
-    loading: false
+    loading: false,
+    openid: "",
+    person_name: "",
+    person_num: 0
+  },
+
+  onReady: function(){
+    this.get_openid();
+    const db = wx.cloud.database();
+    var iflog = false;
+
+    db.collection("user_info")
+      .where({
+        _openid: this.data.openid
+      })
+      .get({
+        success: function(res) {
+          console.log(res);
+          iflog = true;
+        }
+      });
+      if (iflog) {
+        this.setData(
+          {logged:iflog},
+        )
+      }
   },
 
   onLoad: function() {
-    // if (!wx.cloud) {
-    //   wx.redirectTo({
-    //     url: "../chooseLib/chooseLib"
-    //   });
-    //   return;
-    // }
-
-    // // 获取用户信息
-    // wx.getSetting({
-    //   success: res => {
-    //     if (res.authSetting["scope.userInfo"]) {
-    //       // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-    //       wx.getUserInfo({
-    //         success: res => {
-    //           this.setData({
-    //             avatarUrl: res.userInfo.avatarUrl,
-    //             userInfo: res.userInfo
-    //           });
-    //         }
-    //       });
-    //     }
-    //   }
-    // });
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
+    if (!wx.cloud) {
+      wx.redirectTo({
+        url: "../chooseLib/chooseLib"
       });
-    } else if (this.data.canIUse) {
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        });
-      };
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo;
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
+      return;
+    }
+
+    this.get_openid();
+
+    var openid = this.data.openid;
+
+    console.log(this.data.openid);
+
+    const db = wx.cloud.database();
+
+
+    var iflog=false;
+
+    db.collection("user_info")
+      .where({
+        _openid: this.data.openid
+      })
+      .get({
+        success: function(res) {
+          console.log(res);
+          iflog=true;
+        }
+      });
+
+      if(this.data.logged)
+      {
+        console.log("what is wrong?");
+        this.next();
+      }
+    // 获取用户信息
+    wx.getSetting({
+      success: res => {
+        if (res.authSetting["scope.userInfo"]) {
+          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+          wx.getUserInfo({
+            success: res => {
+              this.setData({
+                avatarUrl: res.userInfo.avatarUrl,
+                userInfo: res.userInfo
+              });
+            }
           });
         }
+      }
+    });
+    // if (app.globalData.userInfo) {
+    //   this.setData({
+    //     userInfo: app.globalData.userInfo,
+    //     hasUserInfo: true
+    //   });
+    // } else if (this.data.canIUse) {
+    //   // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+    //   // 所以此处加入 callback 以防止这种情况
+    //   app.userInfoReadyCallback = res => {
+    //     this.setData({
+    //       userInfo: res.userInfo,
+    //       hasUserInfo: true
+    //     });
+    //   };
+    // } else {
+    //   // 在没有 open-type=getUserInfo 版本的兼容处理
+    //   wx.getUserInfo({
+    //     success: res => {
+    //       app.globalData.userInfo = res.userInfo;
+    //       this.setData({
+    //         userInfo: res.userInfo,
+    //         hasUserInfo: true
+    //       });
+    //     }
+    //   });
+    // }
+  },
+
+  onGetUserInfo: function(e) {
+    if (!this.data.logged && e.detail.userInfo) {
+      this.setData({
+        logged: true,
+        avatarUrl: e.detail.userInfo.avatarUrl,
+        userInfo: e.detail.userInfo
       });
     }
   },
-
-  // onGetUserInfo: function(e) {
-  //   if (!this.data.logged && e.detail.userInfo) {
-  //     this.setData({
-  //       logged: true,
-  //       avatarUrl: e.detail.userInfo.avatarUrl,
-  //       userInfo: e.detail.userInfo
-  //     });
-  //   }
+  //函数得到用户信息
+  // getUserInfo: function(e) {
+  //   console.log(e);
+  //   app.globalData.userInfo = e.detail.userInfo;
+  //   this.setData({
+  //     userInfo: e.detail.userInfo,
+  //     hasUserInfo: true
+  //   });
   // },
-//函数得到用户信息
-  getUserInfo: function(e) {
-    console.log(e);
-    app.globalData.userInfo = e.detail.userInfo;
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    });
-  },
 
   inputName: function(name) {
     this.setData({
-      personName: name.detail.value
+      person_name: name.detail.value
     });
-    if (this.data.personName) {
+    if (this.data.person_name) {
       this.setData({ getName: true });
     } else {
       this.setData({ getName: false });
@@ -100,9 +151,9 @@ Page({
 
   inputNum: function(num) {
     this.setData({
-      personNum: num.detail.value
+      person_num: num.detail.value
     });
-    if (this.data.personNum) {
+    if (this.data.person_num) {
       this.setData({ getNum: true });
     } else {
       this.setData({ getNum: false });
@@ -111,19 +162,31 @@ Page({
   },
 
   next: function() {
+    const db = wx.cloud.database();
+    db.collection("user_info").add({
+      data: {
+        _id: this.data.openid,
+        description: "name and number",
+        name: this.data.person_name,
+        number: this.data.person_num,
+        iflogin: true
+      },
+      success: function(res) {
+        console.log(res);
+      }
+    });
     this.setData({ loading: true });
     wx.navigateTo({
-      url:
-        "../homepage/homepage" 
-        // ?name=+
-        // this.data.personName +
-        // "&num=" +
-        // this.data.personNum
+      url: "../homepage/homepage"
+      // ?name=+
+      // this.data.personName +
+      // "&num=" +
+      // this.data.personNum
     });
     this.onLoad();
   },
-  
-  openConfirm: function() {
+
+  open_confirm: function() {
     if (!this.data.getName || !this.data.getNum) {
       var cont = "";
       if (this.data.getName) {
@@ -134,7 +197,7 @@ Page({
         cont = "请输入姓名和学号";
       }
       wx.showModal({
-        title: "弹窗标题",
+        title: "提示",
         content: cont,
         //confirmText: "主操作",
         //cancelText: "辅助操作",
@@ -151,8 +214,37 @@ Page({
     }
     this.onLoad();
   },
-  
-//以下无关
+
+  get_openid: function() {
+    let that = this; //获取openid不需要授权
+    var appid = "wx02976232a5f3e695";
+    var secret = "e35c18d248e228818a136f179b6e2dbb";
+    wx.login({
+      success: function(loginCode) {
+        //请求自己后台获取用户openid
+        wx.request({
+          url:
+            "https://api.weixin.qq.com/sns/jscode2session?appid=" +
+            appid +
+            "&secret=" +
+            secret +
+            "&grant_type=authorization_code&js_code=" +
+            loginCode.code,
+          header: {
+            "content-type": "application/json"
+          },
+          success: function(res) {
+            console.log(res.data.openid); //获取openid
+            that.setData({
+              openid: res.data.openid
+            });
+          }
+        });
+      }
+    });
+  },
+
+  //以下无关
 
   onGetOpenid: function() {
     // 调用云函数
