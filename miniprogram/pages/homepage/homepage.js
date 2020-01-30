ca31318d2ec92d3f0bed8b84dc603b79cff9efd1
swhite-zhang <351168391@qq.com>
2020-01-30 16:14:46 +0800

    edit login page

--- miniprogram/pages/homepage/homepage.js
+++ miniprogram/pages/homepage/homepage.js
@@ -1,50 +1,31 @@
-// miniprogram/pages/homepage/homepage.js
+//homepage.js
+const app = getApp();
 
-/*(1)markers属性 标记点用于在地图上显示标记的位置
-  id  标记点id marker点击事件回调会返回此id。建议为每个marker设置上Number类型id，保证更新marker时有更好的性能。
-  latitude  纬度  浮点数，范围 -90 ~ 90
-  longitude   经度  浮点数，范围 -180 ~ 180
-  title   标注点名 
-  zIndex  显示层级 
-  iconPath  显示的图标   项目目录下的图片路径，支持相对路径写法，以'/'开头则表示相对小程序根目录；也支持临时路径和网络图片（2.3.0）
-  rotate  旋转角度    顺时针旋转的角度，范围 0 ~ 360，默认为 0
-  alpha   标注的透明度  默认1，无透明，范围 0 ~ 1
-  width   标注图标宽度    默认为图片实际宽度，单位px（2.4.0起支持rpx）
-  height  标注图标高度    默认为图片实际高度，单位px（2.4.0起支持rpx）
-  callout   自定义标记点上方的气泡窗口   支持的属性见下表，可识别换行符。 
-  label   为标记点旁边增加标签  支持的属性见下表，可识别换行符。 
-  anchor  经纬度在标注图标的锚点，默认底边中点  {x, y}，x表示横向(0-1)，y表示竖向(0-1)。{x: .5, y: 1} 表示底边中点   
-  aria-label  无障碍访问，（属性）元素的额外描述
-*/
+var amapFile = require("../amap/amap-wx");
 
-/*(2)marker 上的气泡 callout
-  content   文本  
-  color   文本颜色  
-  fontSize  文字大小  
-  borderRadius  边框圆角  
-  borderWidth   边框宽度 
-  borderColor   边框颜色  
-  bgColor   背景色    
-  padding   文本边缘留白 
-  display   'BYCLICK':点击显示; 'ALWAYS':常显    
-  textAlign   文本对齐方式。有效值: left, right, center  
-*/
+var markersData = [];
 
 Page({
-  /**
-   * 页面的初始数据
-   */
   data: {
+    canIUse: wx.canIUse("button.open-type.getUserInfo"),
+    hasUserInfo: false,
+    userInfo: {},
+    avatarUrl: "./user-unlogin.png",
+    logged: false,
+    takeSession: false,
+    requestResult: "",
+    getName: false,
+    loading: false,
+    openid: "",
+    person_name: "",
+    person_num: 0,
+    home: true,
     home: true,
     latitude: "",
     longitude: "",
     scale: 16,
     location: "获取位置",
-    person_num: 0,
-    person_name: "",
-    openid: "",
-    user_info:{},
-    user_background:"",
+    user_background: "",
     markers: [
       {
         id: 1,
@@ -65,68 +46,107 @@ Page({
         //   borderRadius: "5"
         // }
       }
-    ]
-  },
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function(options) {
-    var that=this;
-    that.setData({
-      options: options.openid
-    });
-
-wx.cloud.downloadFile({
-  fileID:
-    "cloud://chuyan-1-co5tb.6368-chuyan-1-co5tb-1301134763/homepage_user_background.jpg", // 文件 ID
-  success: res => {
-    // 返回临时文件路径
-    console.log(res.tempFilePath);
-    that.setData({
-      user_background: res.tempFilePath
-    })
-  },
-  fail: console.error
-});
-
-    wx.getSetting({
-      success: res => {
-          wx.getUserInfo({
-            success: res => {
-              that.setData({
-                user_info: res.userInfo
-              });
-            }
-          });
-        }
-    });
-
-    const db = wx.cloud.database();
-    db.collection("user_info")
-      .where({
-        _openid: that.data.openid
-      })
-      .get({
-        success: res => {
-          that.setData({
-            person_name: res.data[0].name,
-            person_num: res.data[0].number,
-            openid:res.data[0]._openid
-          });
-        }
-      });
+    ],
+    // search_marker: {
+    //   latitude: 0,
+    //   longitude: 0,
+    //   title: "",
+    //   color: "#0xffa500",
+    //   label: ""
+    // },
+    inputShowed: false,
+    inputVal: "",
+    tips: {}
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
   onReady: function(e) {
     this.mapCtx = wx.createMapContext("myMap");
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
+  onLoad: function() {
+    var that = this;
+
+    if (!wx.cloud) {
+      wx.redirectTo({
+        url: "../chooseLib/chooseLib"
+      });
+      return;
+    }
+
+    that.get_openid();
+
+    var openid = that.data.openid;
+
+    console.log(that.data.openid);
+
+    const db = wx.cloud.database();
+
+    // wx.getUserInfo({
+    //   //     // if (res.authSetting["scope.userInfo"]) {
+    //   success: res => {
+    //     console.log(res);
+    //     that.setData({
+    //       userInfo: res.userInfo,
+    //       avatarUrl: res.userInfo.avatarUrl,
+    //       hasUserInfo: true
+    //     });
+    //   },
+    //   fail: console.log("fail to get")
+    // });
+
+    // wx.getSetting({
+    //   success: res => {
+    //     // if (res.authSetting["scope.userInfo"]) {
+    //     // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+    //     wx.getUserInfo({
+    //       success: res => {
+    //         this.setData({
+    //           avatarUrl: res.userInfo.avatarUrl,
+    //           userInfo: res.userInfo
+    //         });
+    //       }
+    //     });
+    //   }
+    // });
+
+    db.collection("userInfo")
+      .where({
+        _openid: this.data.openid
+      })
+      .get({
+        success: function(res) {
+          that.setData({
+            person_name: res.data[0].name,
+            person_num: res.data[0].number,
+            iflogin: res.data[0].iflogin
+          });
+          console.log(res.data);
+        }
+      });
+
+    wx.cloud.downloadFile({
+      fileID:
+        "cloud://chuyan-1-co5tb.6368-chuyan-1-co5tb-1301134763/homepage_user_background.jpg", // 文件 ID
+      success: res => {
+        // 返回临时文件路径
+        console.log(res.tempFilePath);
+        that.setData({
+          user_background: res.tempFilePath
+        });
+      },
+      fail: console.error
+    });
+
+    var myAmapFun = new amapFile.AMapWX({
+      key: "2e4ef5d3b34b3a3167eb9e72ff12e90d"
+    });
+    myAmapFun.getPoiAround({
+      success: function(res) {
+        console.log(res);
+      }
+    });
+  },
+
   onShow: function() {
     var that = this;
     wx.getLocation({
@@ -139,30 +159,128 @@ wx.cloud.downloadFile({
     });
   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function() {},
+  getUserInfo: function(e) {
+    if (!this.data.logged && e.detail.userInfo) {
+      this.setData({
+        logged: true,
+        avatarUrl: e.detail.userInfo.avatarUrl,
+        userInfo: e.detail.userInfo,
+        hasUserInfo: true
+      });
+    }
+  },
+  //函数得到用户信息
+  // getUserInfo: function(e) {
+  //   console.log(e);
+  //   app.globalData.userInfo = e.detail.userInfo;
+  //   this.setData({
+  //     userInfo: e.detail.userInfo,
+  //     hasUserInfo: true
+  //   });
+  // },
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function() {},
+  inputName: function(name) {
+    this.setData({
+      person_name: name.detail.value
+    });
+    if (this.data.person_name) {
+      this.setData({ getName: true });
+    } else {
+      this.setData({ getName: false });
+    }
+    this.onLoad();
+  },
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function() {},
+  inputNum: function(num) {
+    this.setData({
+      person_num: num.detail.value
+    });
+    if (this.data.person_num) {
+      this.setData({ getNum: true });
+    } else {
+      this.setData({ getNum: false });
+    }
+    this.onLoad();
+  },
 
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function() {},
+  next: function() {
+    const db = wx.cloud.database();
+    db.collection("userInfo").add({
+      data: {
+        _id: this.data.openid,
+        description: "name and number",
+        name: this.data.person_name,
+        number: this.data.person_num,
+        iflogin: true
+      },
+      success: function(res) {
+        console.log(res);
+      }
+    });
+    this.setData({ loading: true });
+    // wx.navigateTo({
+    //   url: "../homepage/homepage?openid" + this.data.openid
+    // });
+    this.onLoad();
+  },
 
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function() {},
+  open_confirm: function() {
+    if (!this.data.getName || !this.data.getNum) {
+      var cont = "";
+      if (this.data.getName) {
+        cont = "请输入学号";
+      } else if (this.data.getNum) {
+        cont = "请输入姓名";
+      } else {
+        cont = "请输入姓名和学号";
+      }
+      wx.showModal({
+        title: "提示",
+        content: cont,
+        //confirmText: "主操作",
+        //cancelText: "辅助操作",
+        showCancel: false,
+        success: function(res) {
+          console.log(res);
+          if (res.confirm) {
+            console.log("用户点击主操作");
+          } else {
+            console.log("用户点击辅助操作");
+          }
+        }
+      });
+    }
+    this.onLoad();
+  },
+
+  get_openid: function() {
+    let that = this; //获取openid不需要授权
+    var appid = "wx02976232a5f3e695";
+    var secret = "e35c18d248e228818a136f179b6e2dbb";
+    wx.login({
+      success: function(loginCode) {
+        //请求自己后台获取用户openid
+        wx.request({
+          url:
+            "https://api.weixin.qq.com/sns/jscode2session?appid=" +
+            appid +
+            "&secret=" +
+            secret +
+            "&grant_type=authorization_code&js_code=" +
+            loginCode.code,
+          header: {
+            "content-type": "application/json"
+          },
+          success: function(res) {
+            console.log(res.data.openid); //获取openid
+            that.setData({
+              openid: res.data.openid
+            });
+          }
+        });
+      }
+    });
+  },
 
   home2map: function() {
     this.setData({
@@ -199,44 +317,175 @@ wx.cloud.downloadFile({
       }
     });
   },
-  scaleClick: function() {
+
+  showInput: function() {
     this.setData({
-      scale: 10
+      inputShowed: true
     });
   },
-  // 移动位置
-  moveToLocation: function() {
-    this.mapCtx.moveToLocation();
+  hideInput: function() {
+    this.setData({
+      inputVal: "",
+      inputShowed: false
+    });
   },
-  // 移动标注
-  translateMarker: function() {
-    this.mapCtx.translateMarker({
-      markerId: 1,
-      autoRotate: true,
-      duration: 1000,
-      destination: {
-        latitude: 23.10229,
-        longitude: 113.3345211
-      },
-      animationEnd() {
-        console.log("动画结束");
+  clearInput: function() {
+    this.setData({
+      inputVal: ""
+    });
+  },
+  inputTyping: function(e) {
+    var that = this;
+    that.setData({
+      inputVal: e.detail.value
+    });
+
+    var keywords = e.detail.value;
+    //var key = config.Config.key;
+    var myAmapFun = new amapFile.AMapWX({
+      key: "2e4ef5d3b34b3a3167eb9e72ff12e90d"
+    });
+    myAmapFun.getInputtips({
+      keywords: keywords,
+      location: "",
+      success: function(data) {
+        if (data && data.tips) {
+          that.setData({
+            tips: data.tips
+          });
+          console.log(data.tips);
+        }
       }
     });
   },
-  //缩放视野展示所有经纬度
-  includePoints: function() {
-    this.mapCtx.includePoints({
-      padding: [10],
-      points: [
-        {
-          latitude: 23.10229,
-          longitude: 113.3345211
-        },
-        {
-          latitude: 23.00229,
-          longitude: 113.3345211
-        }
-      ]
+
+  bindSearch: function(e) {
+    var keywords = e.currentTarget.dataset.keywords;
+    console.log(keywords.location);
+    var that = this;
+    var search_marker = {
+      latitude: "",
+      longitude: "",
+      title: "",
+      color: "#0xffa500",
+      label: ""
+    };
+    var i = 0;
+    while (keywords.location[i] != "," && i < 100) {
+      search_marker.longitude += keywords.location[i];
+      i++;
+    }
+    i++;
+    while (i < keywords.location.length) {
+      search_marker.latitude += keywords.location[i];
+      i++;
+    }
+    search_marker.title = keywords.sname;
+    search_marker.label = keywords.name;
+    this.data.markers.push(search_marker);
+    that.setData({
+      inputShowed: false,
+      latitude: search_marker.latitude,
+      longitude: search_marker.longitude
+    });
+    // wx.redirectTo({
+    //   url: url
+    // })
+  },
+
+  showMarkerInfo: function(data, i) {
+    var that = this;
+    that.setData({
+      textData: {
+        name: data[i].name,
+        desc: data[i].address
+      }
+    });
+  },
+  changeMarkerColor: function(data, i) {
+    var that = this;
+    var markers = [];
+    for (var j = 0; j < data.length; j++) {
+      if (j == i) {
+        data[j].iconPath = "选中 marker 图标的相对路径"; //如：..­/..­/img/marker_checked.png
+      } else {
+        data[j].iconPath = "未选中 marker 图标的相对路径"; //如：..­/..­/img/marker.png
+      }
+      markers.push(data[j]);
+    }
+    that.setData({
+      markers: markers
+    });
+  },
+
+  //以下无关
+
+  onGetOpenid: function() {
+    // 调用云函数
+    wx.cloud.callFunction({
+      name: "login",
+      data: {},
+      success: res => {
+        console.log("[云函数] [login] user openid: ", res.result.openid);
+        app.globalData.openid = res.result.openid;
+        wx.navigateTo({
+          url: "../userConsole/userConsole"
+        });
+      },
+      fail: err => {
+        console.error("[云函数] [login] 调用失败", err);
+        wx.navigateTo({
+          url: "../deployFunctions/deployFunctions"
+        });
+      }
+    });
+  },
+
+  // 上传图片
+  doUpload: function() {
+    // 选择图片
+    wx.chooseImage({
+      count: 1,
+      sizeType: ["compressed"],
+      sourceType: ["album", "camera"],
+      success: function(res) {
+        wx.showLoading({
+          title: "上传中"
+        });
+
+        const filePath = res.tempFilePaths[0];
+
+        // 上传图片
+        const cloudPath = "my-image" + filePath.match(/\.[^.]+?$/)[0];
+        wx.cloud.uploadFile({
+          cloudPath,
+          filePath,
+          success: res => {
+            console.log("[上传文件] 成功：", res);
+
+            app.globalData.fileID = res.fileID;
+            app.globalData.cloudPath = cloudPath;
+            app.globalData.imagePath = filePath;
+
+            wx.navigateTo({
+              url: "../storageConsole/storageConsole"
+            });
+          },
+          fail: e => {
+            console.error("[上传文件] 失败：", e);
+            wx.showToast({
+              icon: "none",
+              title: "上传失败"
+            });
+          },
+          complete: () => {
+            wx.hideLoading();
+          }
+        });
+      },
+      fail: e => {
+        console.error(e);
+      }
     });
   }
 });
--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -1,4 +1,5 @@
 //index.js
+//获取应用实例
 const app = getApp();
 
 var amapFile = require("../amap/amap-wx");
@@ -7,19 +8,21 @@ var markersData = [];
 
 Page({
   data: {
+    welcomeBackground:"",
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse("button.open-type.getUserInfo"),
     avatarUrl: "./user-unlogin.png",
-    user_info: {},
     logged: false,
     takeSession: false,
     requestResult: "",
-    canIUse: wx.canIUse("button.open-type.getUserInfo"),
-    getNum: false,
     getName: false,
+    getNum:false,
     loading: false,
     openid: "",
     person_name: "",
     person_num: 0,
-    home: true,
+    iflogin:false,
     home: true,
     latitude: "",
     longitude: "",
@@ -58,13 +61,38 @@ Page({
     inputVal: "",
     tips: {}
   },
-
-  onReady: function(e) {
-    this.mapCtx = wx.createMapContext("myMap");
-  },
-
+  //事件处理函数
   onLoad: function() {
     var that = this;
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      });
+      console.log("getUserInfo");
+    } else if (that.data.canIUse) {
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        });
+      };
+      console.log("getUserInfo");
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo;
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          });
+        }
+      });
+      console.log("getUserInfo");
+    }
 
     if (!wx.cloud) {
       wx.redirectTo({
@@ -81,23 +109,6 @@ Page({
 
     const db = wx.cloud.database();
 
-    // 获取用户信息
-    wx.getSetting({
-      success: res => {
-        if (res.authSetting["scope.userInfo"]) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
-            success: res => {
-              this.setData({
-                avatarUrl: res.userInfo.avatarUrl,
-                user_info: res.userInfo
-              });
-            }
-          });
-        }
-      }
-    });
-
     db.collection("user_info")
       .where({
         _openid: this.data.openid
@@ -107,7 +118,7 @@ Page({
           that.setData({
             person_name: res.data[0].name,
             person_num: res.data[0].number,
-            logged: res.data[0].iflogin
+            iflogin: res.data[0].iflogin
           });
           console.log(res.data);
         }
@@ -125,6 +136,18 @@ Page({
       },
       fail: console.error
     });
+    wx.cloud.downloadFile({
+      fileID:
+        "cloud://chuyan-1-co5tb.6368-chuyan-1-co5tb-1301134763/welcomebackground.jpg", // 文件 ID
+      success: res => {
+        // 返回临时文件路径
+        console.log(res.tempFilePath);
+        that.setData({
+          welcomeBackground: res.tempFilePath
+        });
+      },
+      fail: console.error
+    });
 
     var myAmapFun = new amapFile.AMapWX({
       key: "2e4ef5d3b34b3a3167eb9e72ff12e90d"
@@ -134,34 +157,16 @@ Page({
         console.log(res);
       }
     });
-    // if (app.globalData.userInfo) {
-    //   this.setData({
-    //     userInfo: app.globalData.userInfo,
-    //     hasUserInfo: true
-    //   });
-    // } else if (this.data.canIUse) {
-    //   // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-    //   // 所以此处加入 callback 以防止这种情况
-    //   app.userInfoReadyCallback = res => {
-    //     this.setData({
-    //       userInfo: res.userInfo,
-    //       hasUserInfo: true
-    //     });
-    //   };
-    // } else {
-    //   // 在没有 open-type=getUserInfo 版本的兼容处理
-    //   wx.getUserInfo({
-    //     success: res => {
-    //       app.globalData.userInfo = res.userInfo;
-    //       this.setData({
-    //         userInfo: res.userInfo,
-    //         hasUserInfo: true
-    //       });
-    //     }
-    //   });
-    // }
   },
 
+  getUserInfo: function(e) {
+    console.log(e);
+    app.globalData.userInfo = e.detail.userInfo;
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    });
+  },
   onShow: function() {
     var that = this;
     wx.getLocation({
@@ -174,14 +179,15 @@ Page({
     });
   },
 
-  onGetUserInfo: function(e) {
-    if (!this.data.logged && e.detail.userInfo) {
-      this.setData({
-        logged: true,
-        avatarUrl: e.detail.userInfo.avatarUrl,
-        user_info: e.detail.userInfo
-      });
-    }
+  getUserInfo: function(e) {
+    console.log(e);
+    app.globalData.userInfo = e.detail.userInfo;
+    this.setData({
+      logged: true,
+      avatarUrl: e.detail.userInfo.avatarUrl,
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    });
   },
   //函数得到用户信息
   // getUserInfo: function(e) {
@@ -309,6 +315,7 @@ Page({
   },
 
   back_self: function() {
+    console.log('get location');
     wx.getLocation({
       success: res => {
         this.setData({
@@ -399,8 +406,8 @@ Page({
     this.data.markers.push(search_marker);
     that.setData({
       inputShowed: false,
-      latitude:search_marker.latitude,
-      longitude:search_marker.longitude
+      latitude: search_marker.latitude,
+      longitude: search_marker.longitude
     });
     // wx.redirectTo({
     //   url: url
@@ -432,74 +439,4 @@ Page({
     });
   },
 
-  //以下无关
-
-  onGetOpenid: function() {
-    // 调用云函数
-    wx.cloud.callFunction({
-      name: "login",
-      data: {},
-      success: res => {
-        console.log("[云函数] [login] user openid: ", res.result.openid);
-        app.globalData.openid = res.result.openid;
-        wx.navigateTo({
-          url: "../userConsole/userConsole"
-        });
-      },
-      fail: err => {
-        console.error("[云函数] [login] 调用失败", err);
-        wx.navigateTo({
-          url: "../deployFunctions/deployFunctions"
-        });
-      }
-    });
-  },
-
-  // 上传图片
-  doUpload: function() {
-    // 选择图片
-    wx.chooseImage({
-      count: 1,
-      sizeType: ["compressed"],
-      sourceType: ["album", "camera"],
-      success: function(res) {
-        wx.showLoading({
-          title: "上传中"
-        });
-
-        const filePath = res.tempFilePaths[0];
-
-        // 上传图片
-        const cloudPath = "my-image" + filePath.match(/\.[^.]+?$/)[0];
-        wx.cloud.uploadFile({
-          cloudPath,
-          filePath,
-          success: res => {
-            console.log("[上传文件] 成功：", res);
-
-            app.globalData.fileID = res.fileID;
-            app.globalData.cloudPath = cloudPath;
-            app.globalData.imagePath = filePath;
-
-            wx.navigateTo({
-              url: "../storageConsole/storageConsole"
-            });
-          },
-          fail: e => {
-            console.error("[上传文件] 失败：", e);
-            wx.showToast({
-              icon: "none",
-              title: "上传失败"
-            });
-          },
-          complete: () => {
-            wx.hideLoading();
-          }
-        });
-      },
-      fail: e => {
-        console.error(e);
-      }
-    });
-  }
 });
